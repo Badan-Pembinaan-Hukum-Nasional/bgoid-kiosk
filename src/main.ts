785ef4db3e3437a6da989b90d5dfc8e8d32a211e
Frans Filasta Pratama <fransfilastap@live.com>
2022-09-14 22:52:20 +0700

    UI Tweak

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,29 @@ let date = new Date();
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="wrapper">
-    <h5 class="welcome-text">BPHN Kiosk</h5>
+    <section class="welcome-text">
+        <div class="welcome-text--head">
+            <div>
+            <h5 class="welcome-text--head__greet">Selamat Datang, </h5>
+            <h6 class="welcome-text--head__second">di Badan Pembinaan Hukum Nasional.</h6>
+            </div>
+        </div>
+    </section>
     <section class="video-wrapper">
         <div id="player" data-plyr-provider="youtube" data-plyr-embed-id="p75OXdkxzvc"></div>   
     </section>
     
     <section class="external-links">
-          <figure>
-            <div class="external-links--card">
+          <figure class="external-links--card">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSeozN3ervNQl9Y9a1iVjN7w66z3Dg3_NrCcbuDU4UEGmM6KzA/viewform">
                     <img src="${QrGuestbook}" alt="Link Buku Tamu">
                 </a>
-            </div>
             <figcaption>Buku Tamu</figcaption>
           </figure>
-          <figure>
-            <div class="external-links--card">
+          <figure class="external-links--card">
                 <a href="https://survei.balitbangham.go.id/layanan/450645ee-10d6-11ed-c0e0-323033393538/3ff48bda-4b87-11eb-a51f-323334333033/3e9dbc9e-b776-11e9-b8b2-313834333239/32ce8a8a-e65e-11e9-a50d-313131393436">
                     <img src="${QrSurvey}" alt="Link Survei IPK & IKM">
-                </a>
-            </div>
-            <figcaption>Survei IKM & IPK</figcaption>
+                </a>            <figcaption>Survei IKM & IPK</figcaption>
           </figure>
     </section>
     <section class="touchpoint-links">
