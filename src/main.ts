fdc7bd3d59a3d71485e21a7050777f6c6257094b
Frans Filasta Pratama <fransfilastap@live.com>
2022-09-14 12:31:48 +0700

    Fix plyr controls not placed properly

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,15 @@ import Plyr from "plyr";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="wrapper">
-    <h5 class="welcome-text">Selamat Datang di BPHN</h5>
-  <div id="player" class="video-wrapper" data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+    <h5 class="welcome-text">BPHN Kiosk</h5>
+    <section class="video-wrapper">
+        <div id="player" data-plyr-provider="youtube" data-plyr-embed-id="p75OXdkxzvc"></div>   
+    </section>
+    
     <section class="external-links">
           <figure>
             <div class="external-links--card">
-                <a href="https://google.com">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSeozN3ervNQl9Y9a1iVjN7w66z3Dg3_NrCcbuDU4UEGmM6KzA/viewform">
                     <img src="${QrGuestbook}" alt="Link Buku Tamu">
                 </a>
             </div>
@@ -22,7 +25,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
           </figure>
           <figure>
             <div class="external-links--card">
-                <a href="#">
+                <a href="https://survei.balitbangham.go.id/layanan/450645ee-10d6-11ed-c0e0-323033393538/3ff48bda-4b87-11eb-a51f-323334333033/3e9dbc9e-b776-11e9-b8b2-313834333239/32ce8a8a-e65e-11e9-a50d-313131393436">
                     <img src="${QrSurvey}" alt="Link Survei IPK & IKM">
                 </a>
             </div>
@@ -37,8 +40,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </section>
   </div>`
 
-const player = new Plyr('#player', {});
-
-// Expose player so it can be used from the console
+const player = new Plyr('#player', {
+    autoplay:true,
+    loop:{
+        active: true
+    }
+});
 // @ts-ignore
 window.player = player;
